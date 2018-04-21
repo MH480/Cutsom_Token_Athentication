b7bf3ee9b66ea04a1bb116b9c15aab2e12b9d799
MH <mmsh480@yahoo.com>
2018-04-21 13:41:02 +0430

    somthing to push

--- Models/ViewModels/UserVM.cs
+++ Models/ViewModels/UserVM.cs
@@ -10,9 +10,8 @@ namespace Models.ViewModels
         public string Email { get; set; }
         public string Password { get; set; }
         public string Address { get; set; }
-
         public string PostalCode { get; set; }
-        public string FamilairWith { get; set; }
+        public string FamilierWith { get; set; }
         
     }
 }
